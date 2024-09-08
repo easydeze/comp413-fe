6a28fc50838c3f6302f0e41d75a076a5b2bcd77f
Danny <dz37@rice.edu>
2024-09-08 17:16:51 -0500

    init

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/COMP 413: Stocks/i);
   expect(linkElement).toBeInTheDocument();
 });
