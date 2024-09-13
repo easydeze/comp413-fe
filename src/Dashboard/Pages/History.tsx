9e4b2e8fa735189c71ee00f17d4557a45040541b
Thomas Lee <111800570+tleerice@users.noreply.github.com>
2024-09-12 20:35:32 -0500

    activity tabs

---
+++ src/Dashboard/Pages/History.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const History = () => {
+  return (
+    <>
+      <h1>History</h1>
+      <p>History below.</p>
+    </>
+  );
+};
+
+export default History;
---
+++ src/Dashboard/Pages/Transfers.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Transfers = () => {
+  return (
+    <>
+      <h1>Transfers</h1>
+      <p>Transfers below.</p>
+    </>
+  );
+};
+
+export default Transfers;
