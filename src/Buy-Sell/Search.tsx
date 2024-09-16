83f0a40e042c15425d9201b9a98a4e7aaeb8f36d
mikayBuk <mmb19@rice.edu>
2024-09-16 16:33:02 -0500

    Pushing commits for buy-sell page

---
+++ src/Buy-Sell/Search.tsx
@@ -0,0 +1,15 @@
+//Should display the stock, ticker + company, 
+export default function displayAccount(){
+    //Search bar on top
+
+    //Should have holdings (account amount, day earnings, and day earning %)
+
+    //Graph (Not included in MVP)
+
+    //List of holdings (number of holdings)
+}
+
+function displayHoldings(){
+
+}
+
