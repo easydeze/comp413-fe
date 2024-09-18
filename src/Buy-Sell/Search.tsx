112b12de5768124e5894027d19532800e12fcbfa
mikayBuk <mmb19@rice.edu>
2024-09-17 22:38:07 -0500

    Starting to create Stock Purchase Bar

--- src/Buy-Sell/Search.tsx
+++ src/Buy-Sell/Search.tsx
@@ -1,5 +1,7 @@
+//import Sidebar from "./Sidebar";
+
 //Should display the stock, ticker + company, 
-export default function displayAccount(){
+export default function displayTicker(){
     //Search bar on top
 
     //Should have holdings (account amount, day earnings, and day earning %)
--- src/Dashboard/Pages/BuySell.tsx
+++ src/Dashboard/Pages/BuySell.tsx
@@ -1,7 +1,11 @@
+import StockSidebar from "../../Buy-Sell/Sidebar";
+
+
 export default function BuySell() {
   return (
     <>
       <h2>Buy and Sell</h2>
+      //<StockSidebar/>
     </>
   );
 }
