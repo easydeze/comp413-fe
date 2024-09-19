0809191a8f411afe9795efecc2ec9872d9dbccac
Danny <dz37@rice.edu>
2024-09-19 17:46:46 -0500

    init, have line graph and separate components

--- src/Dashboard/Pages/Home/Balance.tsx
+++ src/Dashboard/Pages/Home/Balance.tsx
@@ -1,7 +1,21 @@
+import { LineChart } from "@mui/x-charts/LineChart";
+
 export default function Balance() {
   return (
-    <>
-      <h1>Balance</h1>
-    </>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <h3>Balance</h3>
+      <div style={{ flex: 1, padding: "10px" }}>
+        <LineChart
+          xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+          series={[
+            {
+              data: [2, 5.5, 2, 8.5, 1.5, 5],
+            },
+          ]}
+          height={700}
+          width={1200}
+        />
+      </div>
+    </div>
   );
 }
