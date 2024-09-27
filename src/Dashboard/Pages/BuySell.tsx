63227e6ca6494f02edfbaedadba028d150ef4dc0
mikayBuk <mmb19@rice.edu>
2024-09-27 11:56:43 -0500

    Basic Desing Finished

--- src/Dashboard/Pages/BuySell.tsx
+++ src/Dashboard/Pages/BuySell.tsx
@@ -1,26 +1,48 @@
+import { Label } from "@mui/icons-material";
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Input, Modal, Typography } from "@mui/material";
-import { Box, style } from "@mui/system";
-import { OrderDialog } from "../../Buy-Sell/Search";
+import { Box } from "@mui/system";
 import React from "react";
-import { log } from "console";
 
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: "white",//'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function BuySell() {
   const [isShown, setIsShown] = React.useState(false);
   const [mode, setMode] = React.useState("NONE");
+  const [ticker, setTicker] = React.useState('');
+  const [numStocks, setStockAmount] = React.useState("");
 
   const handleBuyClick = () => {
     setMode("BUY");
-    setIsShown(true);
+    //setIsShown(true);
   };
 
   const handleSellClick = () => {
     setMode("SELL");
-    setIsShown(true);
+    //setIsShown(true);
   };
+
+  const handleChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setTicker(event.target.value);
+  }
 
   const handleConfirmClick = () => {
     setIsShown(true);
+  };
+
+  const handleStockNumberChange = (event: { target: { value: string; }; }) => {
+    const result = event.target.value.replace(/\D/g, '');
+
+    setStockAmount(result);
   };
 
   const handleClose = () => setIsShown(false);
@@ -29,39 +51,51 @@ export default function BuySell() {
     <Box display="flex" flexDirection="column" alignItems="start">
       <h2>Buy and Sell</h2>
       <Box display="flex" flexDirection="column" alignItems="start">
-        <Input placeholder="Enter Ticker Symbol" />
+        <Box display="flex" flexDirection="row" alignItems="start">
+          <p>Ticker: </p>
+          <Input id="stock-ticker" onChange={handleChange} placeholder="Enter Ticker Symbol" />
+        </Box>
+
         <Box display="flex" flexDirection="row" alignItems="start">
           <Button onClick={handleBuyClick}>Buy</Button>
           {
-            isShown && (
-              <Input placeholder="Buy #"></Input>
+            mode == "BUY" && (
+              <Input onChange={handleStockNumberChange} placeholder="Buy #"></Input>
             )
           }
           <Button onClick={handleSellClick}>Sell</Button>
           {
-            !isShown && (
-              <Input placeholder="Sell #"></Input>
+            mode == "SELL" && (
+              <Input onChange={handleStockNumberChange} placeholder="Sell #"  ></Input>
             )
           }
 
         </Box>
-        <Button onClick={handleConfirmClick}>View Order</Button>
+        <Button variant="contained" onClick={handleConfirmClick}>View Order</Button>
+        {/* TODO:: Make this a DIALOG */}
         <Modal
           open={isShown}
           onClose={handleClose}
           aria-labelledby="modal-modal-title"
           aria-describedby="modal-modal-description"
         >
-          <Box>
+          <Box sx={style}>
             <Typography id="modal-modal-title" variant="h6" component="h2">
               Order Summary
             </Typography>
             <Typography id="modal-modal-descr-mode" sx={{ mt: 2 }}>
               Order Mode: {mode}
             </Typography>
-            <Typography id="modal-modal-descr-total" sx={{ mt: 2 }}>
-              Total Number of Stocks: {1}
+            <Typography id="modal-modal-descr-mode" sx={{ mt: 2 }}>
+              Ticker: {ticker}
             </Typography>
+            <Typography id="modal-modal-descr-total" sx={{ mt: 2 }}>
+              Total Number of Stocks: {numStocks}
+            </Typography>
+            <Box display="flex" flexDirection="row">
+              <Button variant="contained" autoFocus>Confirm</Button>
+              <Button onClick={handleClose}>Cancel</Button>
+            </Box>
           </Box>
         </Modal>
       </Box>
