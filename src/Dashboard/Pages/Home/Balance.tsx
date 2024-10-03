95a62d418f5cb65b1174a59fcab6a753ff38b7bc
Danny <dz37@rice.edu>
2024-10-02 22:39:55 -0500

    add support for balance api

--- src/Dashboard/Pages/Home/Balance.tsx
+++ src/Dashboard/Pages/Home/Balance.tsx
@@ -1,6 +1,18 @@
 import { LineChart } from "@mui/x-charts/LineChart";
+import { useEffect, useState } from "react";
+
+const balance_api = "";
 
 export default function Balance() {
+  // API call to get top 3 movements.
+  let [balances, setBalances] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetch(balance_api)
+      .then((response) => response.json())
+      .then((data) => setBalances(data));
+  }, []);
+
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <h3>Balance</h3>
