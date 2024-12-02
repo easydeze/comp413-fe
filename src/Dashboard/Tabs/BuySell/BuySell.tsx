d447715716913ee755739f8a0a6a417a31915282
mikayBuk <mmb19@rice.edu>
2024-12-01 19:28:27 -0600

    Finishing the pull request

--- src/Dashboard/Tabs/BuySell/BuySell.tsx
+++ src/Dashboard/Tabs/BuySell/BuySell.tsx
@@ -1,36 +1,14 @@
-<<<<<<< HEAD:src/Dashboard/Pages/BuySell.tsx
 
-import { Button, CircularProgress, Dialog, DialogActions, DialogContent, DialogTitle, Input, Modal, TextField, Typography, Unstable_TrapFocus } from "@mui/material";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, Typography } from "@mui/material";
 
-import { Box, Stack } from "@mui/system";
+import { Stack } from "@mui/system";
 import React from "react";
 
-import { Order } from "../../API/Dashboard/BuySellAPI"
-import { buyHttp } from "../../API/Dashboard/BuyAPI"
-import { sellHttp } from "../../API/Dashboard/SellAPI"
-import { getMarketPriceHttp } from "../../API/Dashboard/MarketPriceAPI";
-=======
-import { Button, Input, Modal, Typography } from "@mui/material";
-import { Box } from "@mui/system";
-import React from "react";
+import { Order } from "../../../API/Dashboard/BuySellAPI"
+import { buyHttp } from "../../../API/Dashboard/BuyAPI"
+import { sellHttp } from "../../../API/Dashboard/SellAPI"
+import { getMarketPriceHttp } from "../../../API/Dashboard/MarketPriceAPI";
 
-import { getMarketPrice, makeOrder } from "../../../Model/BuySell_Model";
->>>>>>> bc3d1b954e86ec866e92907887b54da3e8dafb51:src/Dashboard/Tabs/BuySell/BuySell.tsx
-
-const style = {
-  position: "absolute" as "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-<<<<<<< HEAD:src/Dashboard/Pages/BuySell.tsx
-=======
-  bgcolor: "white",
-  border: "2px solid #000",
-  boxShadow: 24,
->>>>>>> bc3d1b954e86ec866e92907887b54da3e8dafb51:src/Dashboard/Tabs/BuySell/BuySell.tsx
-  p: 4,
-};
 
 
 const buttonStyle = {
@@ -38,7 +16,6 @@ const buttonStyle = {
 }
 
 export default function BuySell() {
-<<<<<<< HEAD:src/Dashboard/Pages/BuySell.tsx
   //STATES
   const [isPreviewShown, setIsPreviewShown] = React.useState(false);
   const [isConfirmDisabled, setDisableConfirm] = React.useState(false);
@@ -50,14 +27,6 @@ export default function BuySell() {
   const [numStocks, setStockAmount] = React.useState(0);
   const [limitPrice, setLimitPrice] = React.useState(0);
   const [marketPrice, setMarketPrice] = React.useState(0);
-=======
-  const [isShown, setIsShown] = React.useState(false);
-  const [mode, setMode] = React.useState("NONE");
-  const [ticker, setTicker] = React.useState("");
-  const [numStocks, setStockAmount] = React.useState(0);
-  const [limitPrice, setLimitPrice] = React.useState(0);
-  let marketPrice = 0;
->>>>>>> bc3d1b954e86ec866e92907887b54da3e8dafb51:src/Dashboard/Tabs/BuySell/BuySell.tsx
 
   const handleBuyClick = () => {
     setMode("buy");
@@ -85,43 +54,31 @@ export default function BuySell() {
     target: { value: React.SetStateAction<string> };
   }) => {
     setTicker(event.target.value);
-<<<<<<< HEAD:src/Dashboard/Pages/BuySell.tsx
 
     setTickerError(false)
 
-    getMarketPriceHttp(ticker).then((r) => {
+    getMarketPriceHttp(ticker).then((r: { marketPrice: React.SetStateAction<number>; }) => {
       console.log("Market Price after change for " + ticker)
       console.log(r.marketPrice)
 
       if (typeof r.marketPrice == "number") {
         setTickerError(false)
         setMarketPrice(r.marketPrice)
-=======
-    getMarketPrice(ticker).then((r) => {
-      if (typeof r == "number") {
-        marketPrice = r;
->>>>>>> bc3d1b954e86ec866e92907887b54da3e8dafb51:src/Dashboard/Tabs/BuySell/BuySell.tsx
       } else {
         setTickerError(true)
         setMarketPrice(0)
       }
     });
-<<<<<<< HEAD:src/Dashboard/Pages/BuySell.tsx
 
 
   };
 
-=======
-  };
-
->>>>>>> bc3d1b954e86ec866e92907887b54da3e8dafb51:src/Dashboard/Tabs/BuySell/BuySell.tsx
   const handleLimitPriceChange = (event: {
     target: { value: React.SetStateAction<string> };
   }) => {
     setLimitPrice(Number(event.target.value));
   };
 
-<<<<<<< HEAD:src/Dashboard/Pages/BuySell.tsx
   const handlePreviewClick = async () => {
 
     if (ticker !== "" && numStocks !== 0 && limitPrice !== 0) {
@@ -164,19 +121,6 @@ export default function BuySell() {
     setMode("");
     setStockAmount(0);
     setTicker("");
-=======
-  const handleConfirmClick = () => {
-    if (
-      ticker !== "" &&
-      numStocks !== 0 &&
-      mode !== "NONE" &&
-      limitPrice !== 0
-    ) {
-      setIsShown(true);
-    } else {
-      alert("Enter all required fields.");
-    }
->>>>>>> bc3d1b954e86ec866e92907887b54da3e8dafb51:src/Dashboard/Tabs/BuySell/BuySell.tsx
   };
 
   const handleMakeOrder = async () => {
@@ -247,7 +191,6 @@ export default function BuySell() {
   const handleClose = () => setIsPreviewShown(false);
 
   return (
-<<<<<<< HEAD:src/Dashboard/Pages/BuySell.tsx
     <Stack direction="column" spacing={3}>
 
 
@@ -288,7 +231,7 @@ export default function BuySell() {
       open={isPreviewShown}
       fullWidth={true}
       onClose={(_, reason) => {
-        if(reason != 'backdropClick'){
+        if(reason !== 'backdropClick'){
           setIsPreviewShown(false);
         }
       }}>
@@ -333,95 +276,6 @@ export default function BuySell() {
 
 
     </Stack >
-=======
-    <Box display="flex" flexDirection="column" alignItems="start">
-      <h2>Buy and Sell</h2>
-      <Box display="flex" flexDirection="column" alignItems="start">
-        <Box
-          display="flex"
-          flexDirection="row"
-          alignItems="start"
-          textAlign="center"
-        >
-          <p>Ticker: </p>
-          <Input
-            id="stock-ticker"
-            onChange={handleTickerChange}
-            placeholder="Enter Ticker Symbol"
-          />
-        </Box>
-
-        <Box display="flex" flexDirection="row" alignItems="start">
-          <Button onClick={handleBuyClick}>Buy</Button>
-          {mode === "BUY" && (
-            <Input
-              onChange={handleStockNumberChange}
-              placeholder="Buy #"
-            ></Input>
-          )}
-          <Button onClick={handleSellClick}>Sell</Button>
-          {mode === "SELL" && (
-            <Input
-              onChange={handleStockNumberChange}
-              placeholder="Sell #"
-            ></Input>
-          )}
-        </Box>
-        <Box
-          display="flex"
-          flexDirection="row"
-          alignItems="start"
-          textAlign="center"
-        >
-          <p>Market Price:: {marketPrice}</p>
-          <p>Limit Price:: </p>
-          <Input
-            id="-desired-market-price"
-            onChange={handleLimitPriceChange}
-            placeholder="Enter Desired Price"
-          />
-        </Box>
-
-        <Button variant="contained" onClick={handleConfirmClick}>
-          View Order
-        </Button>
-        {/* TODO:: Make this a DIALOG */}
-        <Modal
-          open={isShown}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Order Summary
-            </Typography>
-            <Typography id="modal-modal-descr-mode" sx={{ mt: 2 }}>
-              Order Mode: {mode}
-            </Typography>
-            <Typography id="modal-modal-descr-mode" sx={{ mt: 2 }}>
-              Ticker: {ticker}
-            </Typography>
-            <Typography id="modal-modal-descr-totalStocks" sx={{ mt: 2 }}>
-              Total Number of Stocks: {numStocks}
-            </Typography>
-            <Typography id="modal-modal-descr-marketPrice" sx={{ mt: 2 }}>
-              Limit Price: ${limitPrice}
-            </Typography>
-            <Typography id="modal-modal-descr-marketPrice" sx={{ mt: 2 }}>
-              Total Price: ${Number(limitPrice) * Number(numStocks)}
-            </Typography>
-            <Box display="flex" flexDirection="row">
-              <Button variant="contained" onClick={handleMakeOrder} autoFocus>
-                Confirm
-              </Button>
-              <Button onClick={handleClose}>Cancel</Button>
-            </Box>
-          </Box>
-        </Modal>
-      </Box>
-    </Box>
->>>>>>> bc3d1b954e86ec866e92907887b54da3e8dafb51:src/Dashboard/Tabs/BuySell/BuySell.tsx
   );
 }
 
