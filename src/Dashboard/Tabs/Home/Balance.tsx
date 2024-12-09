17615ee79d22ac680063f78e0460d105f51cf202
mikayBuk <mmb19@rice.edu>
2024-12-09 17:55:04 -0600

    Attempt getting the balances and movements twice before giving up.

--- src/Dashboard/Tabs/Home/Balance.tsx
+++ src/Dashboard/Tabs/Home/Balance.tsx
@@ -6,6 +6,7 @@ import {
 } from "../../../API/Home/HomeAPI";
 import { Stack } from "@mui/system";
 import { Card, Typography, CardContent, CircularProgress } from "@mui/material";
+import { wait } from "@testing-library/user-event/dist/utils";
 
 export default function Balance() {
   const [total, setTotal] = useState(0);
@@ -13,6 +14,8 @@ export default function Balance() {
   const [balances, setBalances] = useState<number[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(false);
+  const [message, setMessage] = useState("");
+
   const token = sessionStorage.getItem("token");
 
   useEffect(() => {
@@ -34,7 +37,47 @@ export default function Balance() {
           }
         }
       } catch (error) {
-        setError(true);
+        if (token) {
+          //Flag for seeing if next attempts have succeeded
+          var completed = false;
+
+          setError(true);
+          setMessage("Attempting to get balances. Loading...")
+
+          //Try again two more times then set isLoading to false
+          for (let i = 1; i < 3; i++) {
+            console.log("Trynig to get account balance after mishap")
+            try {
+              var response = await homeBalanceHttp(token);
+              var currentBalance = await homeCurrentBalanceHttp(token);
+
+              if (response && currentBalance) {
+                setTimestamps(response.timestamps);
+                setBalances(response.balances);
+                setTotal(currentBalance);
+
+                //Add the balance to the session storage
+                sessionStorage.setItem("balance", currentBalance.toString());
+                setIsLoading(false);
+              }
+
+              completed = true
+
+            } catch (error) {
+              console.error("Attempted " + i + " times");
+            }
+
+          }
+
+          //Check to see if anything was success
+          if (completed) {
+            setIsLoading(false);
+          } else {
+            setIsLoading(false);
+            setMessage("Failed to obtain account balance history")
+          }
+
+        }
       }
     };
 
@@ -42,7 +85,24 @@ export default function Balance() {
   }, [token]);
 
   return error || !timestamps || !balances ? (
-    <div>There is an error getting balance.</div>
+    <Card style={{ margin: "20px", padding: "20px" }}>
+      <Typography variant="h3" gutterBottom>
+        {message}
+      </Typography>
+      <CardContent>
+        <Stack direction="row">
+          {isLoading ? (
+            <div style={{ flex: 1, padding: "10px", textAlign: "center" }}>
+              <CircularProgress />
+            </div>
+          ) : (
+            <div style={{ flex: 1, padding: "10px" }}>
+              {/* Nothing should be displayed */}
+            </div>
+          )}
+        </Stack>
+      </CardContent>
+    </Card>
   ) : (
     <Card style={{ margin: "20px", padding: "20px" }}>
       <Typography variant="h3" gutterBottom>
@@ -58,25 +118,25 @@ export default function Balance() {
               <CircularProgress />
             </div>
           ) : (
-            <div style={{ flex: 1, padding: "10px" }}>
-              <LineChart
-                xAxis={[
-                  {
-                    data: timestamps,
-                    label: "Date",
-                    valueFormatter: (value) => new Date(value).toLocaleString(),
-                  },
-                ]}
-                series={[
-                  {
-                    data: balances,
-                  },
-                ]}
-                height={300}
-                width={600}
-              />
-            </div>
+            <LineChart
+              xAxis={[
+                {
+                  data: timestamps,
+                  label: "Date",
+                  valueFormatter: (value) => new Date(value).toLocaleString(),
+                },
+              ]}
+              series={[
+                {
+                  data: balances,
+                },
+              ]}
+              height={300}
+              width={600}
+            />
           )}
+
+
         </Stack>
       </CardContent>
     </Card>
